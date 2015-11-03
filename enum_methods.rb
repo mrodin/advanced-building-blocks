612bd4aa2142e15e7104e58c59a4c34f8996bea7
Martin Rodin <rodin.martin@gmail.com>
2015-11-03 16:54:31 +0100

    my_each_with_index my_select my_all? my_any?

--- enum_methods.rb
+++ enum_methods.rb
@@ -6,7 +6,49 @@ module Enumerable
   def my_each
     return self unless block_given?
     for i in self
-      yield(i)
+      yield i
     end
   end
+
+  def my_each_with_index
+    return self unless block_given?
+    count = 0
+    for i in self
+      yield i, count
+      count += 1
+    end
+  end
+
+  def my_select
+    return self unless block_given?
+    result = []
+    for i in self
+      result << i if yield i
+    end
+    result
+  end
+
+  def my_all?
+    if block_given?
+      my_each { |i| return false unless yield(i) }
+    else
+      my_each { |i| return false unless i }
+    end
+    true
+  end
+
+  def my_any?
+    if block_given?
+      my_each { |i| return true if yield(i) }
+    else
+      my_each { |i| return true if i }
+    end
+    false
+  end
 end
+
+# %w(a b c d).my_each { |i| puts i }
+# %w(a b c d).my_each_with_index { |i, count| puts "#{count} : #{i}"}
+# puts (1..10).my_select { |i|  i % 3 == 0 }
+# puts %w{ant bear cat}.my_all? {|word| word.length >= 3}
+# puts %w{ant bear cat}.my_any? {|word| word.length >= 3}
